04c194000d00059e7700656463f9530786b16e83
Chan <add7348@outlook.com>
2024-10-16 18:17:14 +0800

    init env(hardhat + foundry)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # loan
+leveraged loan on BASE, which combines uniswap V2 + aave V3
+
+new pair permission from the protocol is required.
+
+available pairs:
+ETH/USDC
---
+++ hardhat.config.ts
@@ -0,0 +1,26 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  solidity: {
+      compilers: [
+          {
+              version: '0.8.27',
+              settings: {
+                  optimizer: {
+                      enabled: true,
+                      runs: 200,
+                  },
+              },
+          },
+      ],
+  },
+
+  gasReporter: {
+    enabled: false,
+  },
+};
+
+export default config;
